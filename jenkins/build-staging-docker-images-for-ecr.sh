555295dba2495d5ed7565d600bd5863acc600488
ergunpala <60260271+ergunpala@users.noreply.github.com>
2025-09-06 23:50:58 +0300

    Update build-staging-docker-images-for-ecr.sh

--- jenkins/build-staging-docker-images-for-ecr.sh
+++ jenkins/build-staging-docker-images-for-ecr.sh
@@ -6,5 +6,5 @@ docker build  -t "${IMAGE_TAG_DISCOVERY_SERVER}" "${WORKSPACE}/spring-petclinic-
 docker build  -t "${IMAGE_TAG_HYSTRIX_DASHBOARD}" "${WORKSPACE}/spring-petclinic-hystrix-dashboard"
 docker build  -t "${IMAGE_TAG_VETS_SERVICE}" "${WORKSPACE}/spring-petclinic-vets-service"
 docker build  -t "${IMAGE_TAG_VISITS_SERVICE}" "${WORKSPACE}/spring-petclinic-visits-service"
-docker build  -t "${IMAGE_TAG_GRAFANA_SERVICE}" "${WORKSPACE}/docker/grafana"
-docker build  -t "${IMAGE_TAG_PROMETHEUS_SERVICE}" "${WORKSPACE}/docker/prometheus"
+# docker build  -t "${IMAGE_TAG_GRAFANA_SERVICE}" "${WORKSPACE}/docker/grafana"
+# docker build  -t "${IMAGE_TAG_PROMETHEUS_SERVICE}" "${WORKSPACE}/docker/prometheus"
